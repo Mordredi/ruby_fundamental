83b10b70c12f5f27171d145f2d708c4f6dc4b2be
Arthur <arthur.p.wright@gmail.com>
2015-04-01 14:14:58 -0400

    Pushed milk to grocery list and built method to iterate over the list

--- exercise6.rb
+++ exercise6.rb
@@ -2,3 +2,16 @@ grocery_list = ["rice", "steak", "corn", "apples"]
 grocery_list.each do |item|
   puts "* #{item}"
 end
+
+grocery_list << "milk"
+grocery_list.each do |item|
+  puts "* #{item}"
+end
+
+def groceries(list)
+  list.each do |item|
+    puts "* #{item}"
+  end
+end
+
+groceries(grocery_list)
