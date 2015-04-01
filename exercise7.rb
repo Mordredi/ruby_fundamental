4fb6dc0fb3940320fec2b90fa57612e54f8fc073
Arthur <arthur.p.wright@gmail.com>
2015-04-01 14:47:04 -0400

    Built method to expand cohort numbers and display.

--- exercise7.rb
+++ exercise7.rb
@@ -15,3 +15,11 @@ cohorts(students)
 students[:cohort4] = 43
 
 puts students.keys
+
+def expanded_cohorts(list)
+  list.each do |key, value|
+    puts "#{key}: #{(value * 1.05).to_i} students. #{(value * 0.05).to_i} new students."
+  end
+end
+
+expanded_cohorts(students)
