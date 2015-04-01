b45c0bf752bdf103ba6bbf756be669dc33750f0e
Arthur <arthur.p.wright@gmail.com>
2015-04-01 14:20:32 -0400

    Check to see if list contains bananas

--- exercise6.rb
+++ exercise6.rb
@@ -17,3 +17,9 @@ end
 groceries(grocery_list)
 
 puts grocery_list.count
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
