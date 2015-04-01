500057b920bc2132fb0c179ae9fc3169c6a945b0
Arthur <arthur.p.wright@gmail.com>
2015-04-01 14:38:30 -0400

    Exercise 7. Built a method to iterate over a hash and display it.

---
+++ exercise7.rb
@@ -0,0 +1,13 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def cohorts(list)
+  list.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+end
+
+cohorts(students)
